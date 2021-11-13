95d22768d2726d3a647428f8df90062e83ab8735
Lartg <roc.gerrety@gmail.com>
2021-11-12 15:38:39 -0900

    added some routes

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
@@ -7,8 +10,19 @@ def home_page():
     """Return homepage."""
     return render_template('home_index.html')
 
-@app.route('/learn-more')
+@app.route('/learn-more/')
 def learn_more():
     return render_template('learn_more.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('create-account')
+def new_account():
+    #submit new account object
+    #log user in
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
